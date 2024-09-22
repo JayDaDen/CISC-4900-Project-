93124ceaf56e64dee6fea89d629b8ebf24ef18dd
AHMED ALI SIDAHMED <121523116+asid1424@users.noreply.github.com>
2024-09-22 14:45:20 -0400

    Create Dashboard.js

---
+++ Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      {/* Future schedule components and other features will be here */}
+    </div>
+  );
+}
+
+export default Dashboard;
