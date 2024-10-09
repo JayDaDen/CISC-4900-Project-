66ae5233bf0385abca6592ff12b9a923c29b2d80
JayDaDen <161236320+JayDaDen@users.noreply.github.com>
2024-10-09 00:56:53 -0400

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import './Login.css';
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -19,23 +20,36 @@ function Login() {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+    <div class = "container">
+
+      <p class="text">Worksite 2024</p>
+        <h1>Welcome Back to Worksite</h1>
+
+        <h2>Please enter your details</h2>
+
+        <p class= "statement">You Work for Us, We Work for You
+          
+        <br></br>
+        <br></br>
+        Schedule viewing and editing made simple.</p>  
+
+     <div class = "form">
+      <form class = "login-form" onSubmit={handleLogin}>
+         <input
+            type="email"
+           placeholder="Email"
+            value={email}
+           onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         <button type="submit">Login</button>
       </form>
+      </div>
     </div>
   );
 }
